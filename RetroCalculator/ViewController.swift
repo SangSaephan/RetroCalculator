b32cc7c7b159c34b19bb3f35042e99d435a3f435
Sang Saephan <sangsaephan510@yahoo.com>
2017-01-02 17:25:57 -0800

    Completed UI, custom font, audio for buttons

---
+++ RetroCalculator/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  RetroCalculator
+//
+//  Created by Sang Saephan on 1/1/17.
+//  Copyright © 2017 Sang Saephan. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    var btnSound: AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundUrl = URL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOf: soundUrl)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+    }
+    
+    @IBAction func numberpressed(sender: UIButton) {
+        playSound()
+    }
+    
+    func playSound() {
+        if btnSound.isPlaying {
+            btnSound.stop()
+        }
+        btnSound.play()
+    }
+
+
+}
+
