e6263726b303d6cd1580c7e2d78ef21998974349
Sang Saephan <sangsaephan510@yahoo.com>
2017-01-02 19:54:45 -0800

    Add math logic and launch screen

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -12,6 +12,22 @@ import AVFoundation
 class ViewController: UIViewController {
 
     var btnSound: AVAudioPlayer!
+    var runningNumber = ""
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    var currentOperation = Operation.Empty
+    var leftValue = ""
+    var rightValue = ""
+    var result = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,10 +41,43 @@ class ViewController: UIViewController {
         } catch let err as NSError {
             print(err.debugDescription)
         }
+        
+        outputLabel.text = "0"
     }
     
     @IBAction func numberpressed(sender: UIButton) {
         playSound()
+        
+        runningNumber += "\(sender.tag)"
+        outputLabel.text = runningNumber
+        
+    }
+    
+    @IBAction func periodPressed(send: UIButton) {
+        playSound()
+        
+        runningNumber += "."
+        outputLabel.text = runningNumber
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(operation: Operation.Divide)
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(operation: Operation.Multiply)
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(operation: Operation.Subtract)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(operation: Operation.Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(operation: currentOperation)
     }
     
     func playSound() {
@@ -36,6 +85,34 @@ class ViewController: UIViewController {
             btnSound.stop()
         }
         btnSound.play()
+    }
+    
+    func processOperation(operation: Operation) {
+        playSound()
+        if currentOperation != Operation.Empty {
+            if runningNumber != "" {
+                rightValue = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValue)! * Double(rightValue)!)"
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValue)! / Double(rightValue)!)"
+                } else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValue)! - Double(rightValue)!)"
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValue)! + Double(rightValue)!)"
+                }
+                
+                leftValue = result
+                outputLabel.text = result
+            }
+            currentOperation = operation
+        } else {
+            leftValue = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
     }
 
 
